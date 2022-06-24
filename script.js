2c79d003431ea9695b3146c1cba0c7e85b600aa3
ShikharSingh98 <shikharsingh98@gmail.com>
2022-06-24 19:24:23 +0530

    feat: added copyright date dynamically

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+//  Navbar Toggling
 const navbarToggleButton = document.querySelector('.navbar-toggle-button');
 const navbarLinks = document.querySelector('.navbar-links');
 
 navbarToggleButton.addEventListener('click', function () {
   navbarLinks.classList.toggle('show-links');
 });
+
+// Dynamically setting Copyright date
+
+const copyrightDate = document.querySelector('.copyright-date');
+
+const currentYear = new Date().getFullYear();
+copyrightDate.textContent = currentYear;
