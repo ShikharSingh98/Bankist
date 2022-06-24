67542be25791ae42f61b558a4e53d468ec50a664
ShikharSingh98 <shikharsingh98@gmail.com>
2022-06-25 00:25:55 +0530

    feat: added dynamic navbar toggling functionality

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 //  Navbar Toggling
 const navbarToggleButton = document.querySelector('.navbar-toggle-button');
+const navbarContainer = document.querySelector('.navbar-container');
 const navbarLinks = document.querySelector('.navbar-links');
 
 navbarToggleButton.addEventListener('click', function () {
-  navbarLinks.classList.toggle('show-links');
+  const navbarContainerHeight = navbarContainer.getBoundingClientRect().height;
+  const linksHeight = navbarLinks.getBoundingClientRect().height;
+  if (navbarContainerHeight === 0) {
+    navbarContainer.style.height = `${linksHeight / 10}rem`;
+  } else {
+    navbarContainer.style.height = `${0}rem`;
+  }
 });
 
 // Dynamically setting Copyright date
